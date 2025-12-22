af7ed0553cd1024065dbd4b84ef6b95079325f06
torchtorchkimtorch <mhkim092929@gmail.com>
2025-12-21 22:49:00 -0800

    fix wbl architecture, add tokenizer copy

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-mergekit-yaml wbl_della.yaml ./merged_models/della-merged --lazy-unpickle --trust-remote-code
+#!/bin/bash
+
+# 출력 경로 설정
+OUTPUT_PATH="./merged_models/tie-merged"
+
+# mergekit 실행
+mergekit-yaml wbl_ties.yaml "$OUTPUT_PATH" --lazy-unpickle --trust-remote-code --cuda --copy-tokenizer
+
+cp /mnt/nlpai-storage/training_team/shared/final_tokenizer/chat_template.jinja "$OUTPUT_PATH/chat_template.jinja"

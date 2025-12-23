831184372d97024a43baf798c5b51601ca3d2eac
torchtorchkimtorch <mhkim092929@gmail.com>
2025-12-22 22:12:02 -0800

    fix shells

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# 출력 경로 설정
-OUTPUT_PATH="./merged_models/tie-merged"
+DEVICE=0
+OUTPUT_PATH="./merged_models"
 
-# mergekit 실행
-mergekit-yaml wbl_ties.yaml "$OUTPUT_PATH" --lazy-unpickle --trust-remote-code --cuda --copy-tokenizer
+CUDA_VISIBLE_DEVICES=$DEVICE mergekit-yaml wbl_ties.yaml "$OUTPUT_PATH" --lazy-unpickle --trust-remote-code --cuda --copy-tokenizer
 
 cp /mnt/nlpai-storage/training_team/shared/final_tokenizer/chat_template.jinja "$OUTPUT_PATH/chat_template.jinja"
